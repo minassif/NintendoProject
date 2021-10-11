6fdc50ed4270005f835c9484eba723228c34456a
Benjamin Lachiver <benjamin.lachiver@gmail.com>
2021-10-11 17:40:32 +0200

    Mise a jour Boutique

--- src/main/java/metier/Boutique.java
+++ src/main/java/metier/Boutique.java
@@ -1,8 +1,45 @@
 package metier;
 
+import java.util.List;
+
 public class Boutique {
 
 	private String nom;
 	private Adresse adresse;
-	private Jeux jeux;
+	private List<Jeu> jeux;
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public Boutique(String nom, Adresse adresse, List<Jeu> jeux) {
+		super();
+		this.nom = nom;
+		this.adresse = adresse;
+		this.jeux = jeux;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public Adresse getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+	public List<Jeu> getJeux() {
+		return jeux;
+	}
+	public void setJeux(List<Jeu> jeux) {
+		this.jeux = jeux;
+	}
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + ", adresse=" + adresse + ", jeux=" + jeux + "]";
+	}
+
+
+	
 }
+
+
