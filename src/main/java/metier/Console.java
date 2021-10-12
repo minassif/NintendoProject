02b8e76a5efa6f42ebbcac317ae409e013519f9c
Achille Monod <achille.monod&@gmail.com>
2021-10-12 09:49:25 +0200

    modif Console

--- src/main/java/metier/Console.java
+++ src/main/java/metier/Console.java
@@ -3,9 +3,16 @@ package metier;
 public class Console {
 
 	private String nom;
+	private int prix;
+	private String dateSortie;
 
-	public Console(String nom) {
+	
+
+	public Console(String nom, int prix, String dateSortie) {
+		super();
 		this.nom = nom;
+		this.prix = prix;
+		this.dateSortie = dateSortie;
 	}
 
 	public String getNom() {
@@ -15,11 +22,30 @@ public class Console {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public String getDateSortie() {
+		return dateSortie;
+	}
+
+	public void setDateSortie(String dateSortie) {
+		this.dateSortie = dateSortie;
+	}
 
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", dateSortie=" + dateSortie + "]";
 	}
+
+	
 	
 	
 }
